0a5cb5cdd1c81ce60773efe027e3fbb8bef61ce7
naorgenis <naorgenis@gmail.com>
2021-04-30 13:20:05 +0300

    Initial commit

--- src/js/controller.js
+++ src/js/controller.js
@@ -140,7 +140,6 @@ const init = function () {
   searchView.addHandlerSearch(controlSearchResults);
   paginationView.addHandlerClick(controlPagination);
   addRecipeView.addhandlerUpload(controlAddNewRecipe);
-  console.log('hello');
 };
 
 init();
